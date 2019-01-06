1a6f4b2cc06f1f0a1e75c47199f0fe2b110e6d8b
Brian Dillingham <bdillingham88@gmail.com>
2019-01-06 17:45:23 -0500

    wip

--- config/nova-button.php
+++ config/nova-button.php
@@ -9,7 +9,7 @@ return [
     |
     | This key value pair allows you to override the default nova button styles
     | and add entirely new style key value pairs altogether that you need.
-    | The css classes below are Tailwind-CSS  that are come with Nova.
+    | The css classes below are Tailwind-CSS that are come with Nova.
     |
     */
 
