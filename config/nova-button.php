eeb6a9978f27183c527150a8db95c004c800b910
Brian Dillingham <bdillingham88@gmail.com>
2019-01-06 16:55:22 -0500

    wip

--- config/nova-button.php
+++ config/nova-button.php
@@ -15,10 +15,12 @@ return [
 
     'styles' => [
         'link' => 'cursor-pointer dim inline-block text-primary font-bold',
+        'success' => 'btn btn-default btn-success',
         'primary' => 'btn btn-default btn-primary',
         'warning' => 'btn btn-default bg-warning text-white',
         'danger' => 'btn btn-default bg-danger text-white',
         'info' => 'btn btn-default bg-info text-white',
+        'success-outline' => 'btn btn-default border border-success text-success',
         'primary-outline' => 'btn btn-default border border-primary text-primary',
         'warning-outline' => 'btn btn-default border border-warning text-warning ',
         'danger-outline' => 'btn btn-default border border-danger text-danger ',
