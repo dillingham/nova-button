88f97c26ba7e94d0d2a18dd9a9f34317d2dc746e
Brian Dillingham <bdillingham88@gmail.com>
2019-01-15 10:14:34 -0500

    add detail reload

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex border-b border-40 nova-button-wrapper" v-if="visible">
+    <div class="flex border-b border-40 nova-button-wrapper" v-if="field.visible">
         <div class="w-1/4 py-4">
             <label class="font-normal text-80">{{ field.label }}</label>
         </div>
         <div class="w-3/4 py-4">
-            <nova-button v-bind="$props" @clicked="handleClick" />
+            <nova-button v-bind="$props" @finished="reload" />
         </div>
     </div>
 </template>
@@ -12,16 +12,12 @@
 <script>
 export default {
     props: ['resource', 'resourceName', 'resourceId', 'field'],
-    data: () => ({
-        visible: true
-    }),
-    methods: {
-        handleClick() {
-            
-            this.visible = false
-            
-            if(this.field.reload) {
-                this.$router.go()
+     methods: {
+         reload()  {
+            if(this.field.reload && queue.allowsReload()) {
+                window.setTimeout(() => {
+                    this.$router.go()
+                }, 400)
             }
         }
     }
