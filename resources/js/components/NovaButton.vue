c5966c692802e0f780835b01fa060b3631d6d093
Brian Dillingham <bdillingham88@gmail.com>
2019-04-18 07:53:28 -0400

    prevent double click - closes #25

--- resources/js/components/NovaButton.vue
+++ resources/js/components/NovaButton.vue
@@ -1,142 +1,132 @@
 <template>
-<span v-if="field.visible">
+  <span v-if="field.visible">
     <router-link
-        v-if="field.type == 'route'"
-        :to="field.route"
-        ref="novabutton"
-        class="nova-button"
-        v-html="buttonText"
-        :class="buttonClasses"
-        :style="{'min-width': buttonWidth}"
+      v-if="field.type == 'route'"
+      :to="field.route"
+      ref="novabutton"
+      class="nova-button"
+      v-html="buttonText"
+      :class="buttonClasses"
+      :style="{'min-width': buttonWidth}"
     />
     <a
-        v-else-if="field.type == 'link'"
-        ref="novabutton"
-        v-html="buttonText"
-        class="nova-button"
-        :class="buttonClasses"
-        :href="field.link.href"
-        :target="field.link.target"
-        :style="{'min-width': buttonWidth}"
+      v-else-if="field.type == 'link'"
+      ref="novabutton"
+      v-html="buttonText"
+      class="nova-button"
+      :class="buttonClasses"
+      :href="field.link.href"
+      :target="field.link.target"
+      :style="{'min-width': buttonWidth}"
     />
     <span v-else :class="ajaxClasses">
-        <a
-            ref="novabutton"
-            v-html="buttonText"
-            @click="handleClick"
-            class="nova-button"
-            :class="buttonClasses"
-            :style="{'min-width': buttonWidth}"
-        />
+      <a
+        ref="novabutton"
+        v-html="buttonText"
+        @click.once="handleClick"
+        class="nova-button"
+        :class="buttonClasses"
+        :style="{'min-width': buttonWidth}"
+      />
     </span>
-</span>
+  </span>
 </template>
 
 <style>
-    .nova-button-loading,
-    .nova-button-success,
-    .nova-button-error {
-        pointer-events: none;
-    }
+.nova-button-loading,
+.nova-button-success,
+.nova-button-error {
+  pointer-events: none;
+}
 </style>
 
 <script>
-
-import { queue } from '../queue.js';
+import { queue } from "../queue.js";
 
 export default {
-    props: ['resource', 'resourceName', 'resourceId', 'field'],
-    data: () => ({
-        buttonWidth: null,
-        loading: false,
-        success: false,
-        error: false,
-    }),
-    mounted()
-    {
-        this.$nextTick(function(){
-            this.buttonWidth = this.$refs.novabutton.clientWidth + 2 + 'px';
-        })
+  props: ["resource", "resourceName", "resourceId", "field"],
+  data: () => ({
+    buttonWidth: null,
+    loading: false,
+    success: false,
+    error: false
+  }),
+  mounted() {
+    this.$nextTick(function() {
+      this.buttonWidth = this.$refs.novabutton.clientWidth + 2 + "px";
+    });
+  },
+  methods: {
+    async handleClick() {
+      queue.add(this.resourceId);
+
+      this.$emit("clicked");
+
+      try {
+        const response = await this.post();
+        this.success = true;
+        this.loading = false;
+        queue.hasSuccess = true;
+        queue.remove(this.resourceId);
+        this.$emit("success");
+        this.$emit("finished");
+      } catch (error) {
+        this.error = true;
+        this.loading = false;
+        queue.hasError = true;
+        queue.remove(this.resourceId);
+        this.$emit("error");
+        this.$emit("finished");
+      }
     },
-    methods: {
-        async handleClick() {
+    post() {
+      this.$emit("loading");
 
-            queue.add(this.resourceId)
+      window.setTimeout(() => {
+        this.loading = true;
+      }, 200);
 
-            this.$emit('clicked');
+      let root = "/nova-vendor/nova-button/";
 
-            try {
-                const response = await this.post();
-                this.success = true;
-                this.loading = false;
-                queue.hasSuccess = true;
-                queue.remove(this.resourceId);
-                this.$emit('success');
-                this.$emit('finished');
-            } catch (error) {
-                this.error = true;
-                this.loading = false;
-                queue.hasError = true;
-                queue.remove(this.resourceId);
-                this.$emit('error');
-                this.$emit('finished');
-            }
-        },
-        post()
+      return Nova.request().post(
+        root + `${this.resourceName}/${this.resourceId}/${this.field.key}`,
         {
-            this.$emit('loading');
-
-            window.setTimeout(() => {
-                this.loading = true;
-            }, 200)
-
-            let root = '/nova-vendor/nova-button/';
-
-            return Nova.request()
-                .post(root + `${this.resourceName}/${this.resourceId}/${this.field.key}`, {
-                    event: this.field.event
-                })
+          event: this.field.event
         }
-    },
-    computed: {
-        buttonText: function() {
-
-            if(this.error && this.field.errorText)
-            {
-                return this.field.errorText;
-            }
-
-            if(this.success && this.field.successText)
-            {
-                return this.field.successText;
-            }
-
-            if(this.loading && this.field.loadingText)
-            {
-                return this.field.loadingText;
-            }
-
-            return this.field.text;
-        },
-        buttonClasses: function() {
-
-            if(this.error && this.field.errorClasses.length)
-            {
-                return this.field.errorClasses + ' text-center nova-button-error';
-            }
-
-            if(this.success && this.field.successClasses.length)
-            {
-                return this.field.successClasses + ' text-center nova-button-success';
-            }
-
-            if(this.loading && this.field.loadingClasses)
-            {
-                return this.field.loadingClasses + ' text-center nova-button-loading';
-            }
-
-            return this.field.classes;
-        }
+      );
     }
-}
+  },
+  computed: {
+    buttonText: function() {
+      if (this.error && this.field.errorText) {
+        return this.field.errorText;
+      }
+
+      if (this.success && this.field.successText) {
+        return this.field.successText;
+      }
+
+      if (this.loading && this.field.loadingText) {
+        return this.field.loadingText;
+      }
+
+      return this.field.text;
+    },
+    buttonClasses: function() {
+      if (this.error && this.field.errorClasses.length) {
+        return this.field.errorClasses + " text-center nova-button-error";
+      }
+
+      if (this.success && this.field.successClasses.length) {
+        return this.field.successClasses + " text-center nova-button-success";
+      }
+
+      if (this.loading && this.field.loadingClasses) {
+        return this.field.loadingClasses + " text-center nova-button-loading";
+      }
+
+      return this.field.classes;
+    }
+  }
+};
 </script>
