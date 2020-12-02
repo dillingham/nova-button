2e56c43a84378ff30c827991778de686e7ec866b
Lucas Dalmarco <dalmarcolucas@gmail.com>
2020-12-02 10:19:09 -0300

    ajuste

--- resources/js/field.js
+++ resources/js/field.js
@@ -2,5 +2,5 @@ Nova.booting((Vue, router) => {
     Vue.component('nova-button', require('./components/NovaButton').default);
     Vue.component('index-nova-button', require('./components/IndexField').default);
     Vue.component('detail-nova-button', require('./components/DetailField').default);
-    Vue.component('form-nova-button', require('./components/FormField'));
+    Vue.component('form-nova-button', require('./components/FormField').default);
 })
